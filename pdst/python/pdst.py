0ca91d20ea3ad6d30e691c28c00f96fcb8eb7d77
Scott Warchal <s.warchal@sms.ed.ac.uk>
2016-10-13 13:41:21 +0100

    more code-golf

--- pdst/python/pdst.py
+++ pdst/python/pdst.py
@@ -7,27 +7,24 @@ from Bio import SeqIO
 def read_fasta(file_path):
     """ read fasta file """
     handle = open(file_path, "r")
-    sequences = [str(record.seq) for record in SeqIO.parse(handle, "fasta")]
-    return sequences
+    return [str(record.seq) for record in SeqIO.parse(handle, "fasta")]
 
 
 def pdist(s1, s2):
     """ proportional distance between two strings """
     assert len(s1) == len(s2)
-    return sum([i != j for i,j in zip(s1, s2)]) / len(s1)
+    return sum(i != j for i,j in zip(s1, s2)) / len(s1)
 
 
 def pairwise_pdist(x):
     """ slow pairwise pdist between a list of strings """
-    out = [pdist(i, j) for i in x for j in x]
-    out_arr = np.array(out)
-    out_arr.shape = (len(x), len(x))
-    return out_arr
+    out = np.array([pdist(i, j) for i in x for j in x])
+    out.shape = (len(x), len(x))
+    return out
 
 
 if __name__ == "__main__":
-
     seqs = read_fasta(argv[1])
     ans = pairwise_pdist(seqs)
-    np.savetxt("ans.txt", ans, fmt='%5f', delimiter=' ')
+    np.savetxt("ans.txt", ans, fmt='%2f', delimiter=' ')
 
