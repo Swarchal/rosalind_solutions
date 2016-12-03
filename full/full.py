919106cc6e3cc26ba8161857c9878b43943d8da8
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-12-03 14:37:16 +0000

    better solution

--- full/full.py
+++ full/full.py
@@ -1,35 +1,55 @@
 #!/usr/bin/env python3
 
-import data
+"""rosalind/problems/full"""
+
+from data import fetch_mass_dict
+import itertools
 from sys import argv
 
-mass_dict = data.fetch_mass_dict()
+mass_dict = fetch_mass_dict()
 
-def find_match(current, mass_list):
-    """return first match from list of masses"""
-    for i in mass_list:
-        for aa, mass in mass_dict.items():
-            if abs(mass - (i - current)) < 1e-3:
-                return aa
-    return "abort abort!"
+def find_pairs(parent, ions):
+    """find pairs of b-ion and y-ion that equal mass of parent"""
+    pairs = []
+    for b, y in itertools.product(ions, repeat=2):
+        if (2 * b < parent) and (abs(parent - b - y) < 1e-3):
+            pairs.append((b, y))
+    return pairs
+
+
+def flip_and_sort(pairs):
+    """reverse b-ion/y-ion in first pair and sort the list on b-ion mass"""
+    pairs[1] = tuple(reversed(pairs[1]))
+    return sorted(pairs)
+
+
+def b_ion_diff(b, c):
+    """
+    compare difference between two b-ions and determine if this difference
+    close enough to match the mass of an amino acid.
+    """
+    diff = abs(b - c)
+    for aa, mass in mass_dict.items():
+        if abs(diff - mass) < 1e-3:
+            return aa
+    raise Exception()
 
 
 def main():
-    parent, current, *rest = map(float, open(argv[1]))
+    """return protein string"""
+    parent, *ions = map(float, open(argv[1]))
+    pairs = find_pairs(parent, ions)
     seq = ""
-    n = (len([current] + rest) - 2) / 2
+    n = (len(ions)-2) / 2
     while len(seq) < n:
-        tmp = find_match(current, rest)
-        if tmp == "abort abort!":
-            break
-        else:
-            seq += tmp
-            current += mass_dict[tmp]
-            print(current)
-            rest = [i for i in rest if i - current > 0]
+        b1, b2 = pairs[0][0], pairs[1][0]
+        try:
+            seq += b_ion_diff(b1, b2) 
+            pairs.pop(0)
+            pairs = sorted(pairs)
+        except:
+            pairs = flip_and_sort(pairs)
     print(seq)
-
 
 if __name__ == "__main__":
     main()
-
