581ef9e92f3bbdee6861a5e555ff614ff18fac22
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-01-30 18:47:40 +0000

    more code golf

--- kmer/kmer.py
+++ kmer/kmer.py
@@ -3,13 +3,12 @@ import re
 from sys import argv
 from itertools import product
 
-def count_kmer(string, kmer):
-    return len(re.findall(f"(?={kmer})", string))
+count = lambda seq, kmer: len(re.findall(f"(?={kmer})", seq))
 
 def main():
-    seq = "".join(i.strip() for i in open(argv[1]).readlines() if not i.startswith(">"))
+    seq = "".join(i.strip() for i in open(argv[1]).readlines()[1:])
     all_kmers = ["".join(j) for j in product(["A", "C", "G", "T"], repeat=4)]
-    ans = [count_kmer(seq, kmer) for kmer in all_kmers]
+    ans = [count(seq, kmer) for kmer in all_kmers]
     print(*ans)
 
 if __name__ == "__main__":
