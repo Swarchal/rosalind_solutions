821fb58164c0a1c2caca0aa13f3f371aeb85d0bd
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-01-30 18:38:24 +0000

    code golf

--- kmer/kmer.py
+++ kmer/kmer.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3.6
-# rosalind/kmer
-import sys
 import re
+from sys import argv
 from itertools import product
-from Bio import SeqIO
 
 def count_kmer(string, kmer):
-    """use regex lookaheads to count overlapping matches"""
     return len(re.findall(f"(?={kmer})", string))
 
-
-def get_seq(path):
-    """return single sequence from fasta"""
-    fasta = SeqIO.parse(path, "fasta")
-    for i in fasta:
-        return str(i.seq)
-
-
 def main():
-    seq = get_seq(sys.argv[1])
-    all_kmers = ["".join(i) for i in product(["A", "C", "G", "T"], repeat=4)]
+    seq = "".join(i.strip() for i in open(argv[1]).readlines() if not i.startswith(">"))
+    all_kmers = ["".join(j) for j in product(["A", "C", "G", "T"], repeat=4)]
     ans = [count_kmer(seq, kmer) for kmer in all_kmers]
     print(*ans)
 
